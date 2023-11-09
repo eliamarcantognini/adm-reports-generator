4ecc7421d20ff8b77fc2da548adb24c09c052dcd
Elia Marcantognini <elia.marcantognini@gmail.com>
2023-11-09 10:43:15 +0100

    Create Raccolta.py

---
+++ server/models/Raccolta.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+class RaccoltaModel(BaseModel):
+    denominazioneEsercizio: str = "",
+    indirizzoEsercizio: str = "",
+    picfAttivita: str = "",
+    nPuntoRaccolta: str = "",
+    nConcessione: str = "",
+    nomeConcessionario: str = "",
+    titoloAutorizzatorio: str = "",
+    dataTitoloAutorizzatorio: str = "",
+    dataInizioScommesse: str = "",
+    dataFineScommesse: str = "",
+    verbalizzante1: str = "",
+    verbalizzante2: str = "",
+    verbalizzante3: str = "",
+    ordineDiAccesso: str = "",
+    dataVerifica: str = ""
